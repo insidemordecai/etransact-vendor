aaf133f75d520553e8cd51ff6474a9cd3b6fec2a
itsmordecai <mordecai.kipngetich@gmail.com>
2022-01-26 13:10:35 +0300

    update: added support for tab/enter button and show/hide password

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -21,8 +21,10 @@ class _LogInState extends State<LogIn> {
   final String title = 'eTransact';
   final String subTitle = 'Vendor';
 
+  final _formKey = GlobalKey<FormState>();
   final _auth = FirebaseAuth.instance;
   bool showSpinner = false;
+  bool _isHidden = true;
 
   late String email;
   late String password;
@@ -32,119 +34,100 @@ class _LogInState extends State<LogIn> {
     return Scaffold(
       body: ModalProgressHUD(
         inAsyncCall: showSpinner,
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 24.0),
-          child: Center(
-            child: SingleChildScrollView(
-              child: SizedBox(
-                width: 500.0,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          title,
-                          style: const TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 50,
-                              fontFamily: 'Roboto'),
+        child: Form(
+          key: _formKey,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 24.0),
+            child: Center(
+              child: SingleChildScrollView(
+                child: SizedBox(
+                  width: 500.0,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              title,
+                              style: const TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 50,
+                                  fontFamily: 'Roboto'),
+                            ),
+                            const SizedBox(
+                              width: 12.0,
+                            ),
+                            Text(
+                              subTitle,
+                              style: const TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 50,
+                                fontFamily: 'Roboto',
+                                color: Palette.kTeal,
+                              ),
+                            ),
+                          ],
                         ),
-                        const SizedBox(
-                          width: 12.0,
+                      ),
+                      const SizedBox(
+                        height: 32.0,
+                      ),
+                      TextFormField(
+                        keyboardType: TextInputType.emailAddress,
+                        textCapitalization: TextCapitalization.none,
+                        textInputAction: TextInputAction.next,
+                        onChanged: (value) {
+                          email = value;
+                        },
+                        decoration: kTextFieldDecoration.copyWith(
+                          hintText: 'Email',
                         ),
-                        Text(
-                          subTitle,
-                          style: const TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 50,
-                            fontFamily: 'Roboto',
-                            color: Palette.kTeal,
+                      ),
+                      const SizedBox(
+                        height: 24.0,
+                      ),
+                      TextFormField(
+                        obscureText: _isHidden,
+                        textInputAction: TextInputAction.done,
+                        onChanged: (value) {
+                          password = value;
+                        },
+                        onFieldSubmitted: (value) {
+                          performLogIn();
+                        },
+                        decoration: kTextFieldDecoration.copyWith(
+                          hintText: 'Password',
+                          suffixIcon: Padding(
+                            padding:
+                                const EdgeInsets.symmetric(horizontal: 15.0),
+                            child: IconButton(
+                              onPressed: _togglePasswordView,
+                              icon: Icon(
+                                _isHidden
+                                    ? Icons.visibility
+                                    : Icons.visibility_off,
+                              ),
+                            ),
                           ),
                         ),
-                      ],
-                    ),
-                    const SizedBox(
-                      height: 32.0,
-                    ),
-                    TextField(
-                      keyboardType: TextInputType.emailAddress,
-                      textAlign: TextAlign.center,
-                      onChanged: (value) {
-                        email = value;
-                      },
-                      decoration: kTextFieldDecoration.copyWith(
-                        hintText: 'Enter your email',
                       ),
-                    ),
-                    const SizedBox(
-                      height: 24.0,
-                    ),
-                    TextField(
-                      obscureText: true,
-                      textAlign: TextAlign.center,
-                      onChanged: (value) {
-                        password = value;
-                      },
-                      decoration: kTextFieldDecoration.copyWith(
-                        hintText: 'Enter your password',
+                      const SizedBox(
+                        height: 30.0,
                       ),
-                    ),
-                    const SizedBox(
-                      height: 30.0,
-                    ),
-                    ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        shape: kRoundedBorder,
-                        fixedSize: kFixedSize,
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          shape: kRoundedBorder,
+                          fixedSize: kFixedSize,
+                        ),
+                        onPressed: performLogIn,
+                        child: const Text('Log In'),
                       ),
-                      onPressed: () async {
-                        setState(() {
-                          showSpinner = true;
-                        });
-                        try {
-                          await _auth.signInWithEmailAndPassword(
-                            email: email,
-                            password: password,
-                          );
-                          User? loggedUser = FirebaseAuth.instance.currentUser;
-                          Navigator.pushAndRemoveUntil(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) =>
-                                      Home(uid: loggedUser!.uid)),
-                              (Route<dynamic> route) => false);
-                          setState(() {
-                            showSpinner = false;
-                          });
-                        } on FirebaseAuthException catch (e) {
-                          if (e.code == 'user-not-found') {
-                            setState(() {
-                              showSpinner = false;
-                            });
-
-                            kShowToast(
-                                'No user found for that email. Re-enter credentials');
-                          } else if (e.code == 'wrong-password') {
-                            setState(() {
-                              showSpinner = false;
-                            });
-
-                            kShowToast('Wrong password. Re-enter credentials');
-                          } else {
-                            setState(() {
-                              showSpinner = false;
-                            });
-
-                            kShowToast('Error. Try again!');
-                          }
-                        }
-                      },
-                      child: const Text('Log In'),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               ),
             ),
@@ -152,5 +135,53 @@ class _LogInState extends State<LogIn> {
         ),
       ),
     );
+  }
+
+  void _togglePasswordView() {
+    setState(() {
+      _isHidden = !_isHidden;
+    });
+  }
+
+  performLogIn() async {
+    setState(() {
+      showSpinner = true;
+    });
+    try {
+      await _auth.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      User? loggedUser = FirebaseAuth.instance.currentUser;
+
+      Navigator.pushAndRemoveUntil(
+          context,
+          MaterialPageRoute(builder: (context) => Home(uid: loggedUser!.uid)),
+          (Route<dynamic> route) => false);
+
+      setState(() {
+        showSpinner = false;
+      });
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'user-not-found') {
+        setState(() {
+          showSpinner = false;
+        });
+
+        kShowToast('No user found for that email. Re-enter credentials');
+      } else if (e.code == 'wrong-password') {
+        setState(() {
+          showSpinner = false;
+        });
+
+        kShowToast('Wrong password. Re-enter credentials');
+      } else {
+        setState(() {
+          showSpinner = false;
+        });
+
+        kShowToast('Error. Try again!');
+      }
+    }
   }
 }
--- lib/screens/signup.dart
+++ lib/screens/signup.dart
@@ -28,6 +28,7 @@ class _SignUpState extends State<SignUp> {
 
   final _formKey = GlobalKey<FormState>();
   bool showSpinner = false;
+  bool _isHidden = true;
 
   late String email;
   late String password;
@@ -50,41 +51,45 @@ class _SignUpState extends State<SignUp> {
                     mainAxisAlignment: MainAxisAlignment.center,
                     crossAxisAlignment: CrossAxisAlignment.center,
                     children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text(
-                            title,
-                            style: const TextStyle(
+                      Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              title,
+                              style: const TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 50,
+                                  fontFamily: 'Roboto'),
+                            ),
+                            const SizedBox(
+                              width: 12.0,
+                            ),
+                            Text(
+                              subTitle,
+                              style: const TextStyle(
                                 fontWeight: FontWeight.bold,
                                 fontSize: 50,
-                                fontFamily: 'Roboto'),
-                          ),
-                          const SizedBox(
-                            width: 12.0,
-                          ),
-                          Text(
-                            subTitle,
-                            style: const TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 50,
-                              fontFamily: 'Roboto',
-                              color: Palette.kTeal,
+                                fontFamily: 'Roboto',
+                                color: Palette.kTeal,
+                              ),
                             ),
-                          ),
-                        ],
+                          ],
+                        ),
                       ),
                       const SizedBox(
                         height: 32.0,
                       ),
                       // using TextFormField (instead of TextField) to make use of validate operations
                       TextFormField(
-                        textAlign: TextAlign.center,
+                        keyboardType: TextInputType.name,
+                        textInputAction: TextInputAction.next,
                         onChanged: (value) {
                           name = value;
                         },
                         decoration: kTextFieldDecoration.copyWith(
-                          hintText: 'Enter vendor name',
+                          hintText: 'Vendor Name',
                         ),
                         validator: (value) {
                           if (value!.isEmpty) {
@@ -98,12 +103,13 @@ class _SignUpState extends State<SignUp> {
                       ),
                       TextFormField(
                         keyboardType: TextInputType.emailAddress,
-                        textAlign: TextAlign.center,
+                        textCapitalization: TextCapitalization.none,
+                        textInputAction: TextInputAction.next,
                         onChanged: (value) {
                           email = value;
                         },
                         decoration: kTextFieldDecoration.copyWith(
-                          hintText: 'Enter your email',
+                          hintText: 'Email',
                         ),
                         validator: (value) {
                           if (value!.isEmpty) {
@@ -119,16 +125,31 @@ class _SignUpState extends State<SignUp> {
                       ),
                       TextFormField(
                         obscureText: true,
-                        textAlign: TextAlign.center,
+                        textInputAction: TextInputAction.done,
                         onChanged: (value) {
                           password = value;
                         },
+                        onFieldSubmitted: (value) {
+                          performSignUp();
+                        },
                         decoration: kTextFieldDecoration.copyWith(
-                          hintText: 'Enter your password',
+                          hintText: 'Password',
+                          suffixIcon: Padding(
+                            padding:
+                                const EdgeInsets.symmetric(horizontal: 15.0),
+                            child: IconButton(
+                              onPressed: _togglePasswordView,
+                              icon: Icon(
+                                _isHidden
+                                    ? Icons.visibility
+                                    : Icons.visibility_off,
+                              ),
+                            ),
+                          ),
                         ),
                         validator: (value) {
                           if (value!.isEmpty) {
-                            return 'Enter Password';
+                            return 'Enter password';
                           } else if (value.length < 6) {
                             return 'Password must be at least 6 characters!';
                           }
@@ -143,46 +164,7 @@ class _SignUpState extends State<SignUp> {
                           shape: kRoundedBorder,
                           fixedSize: kFixedSize,
                         ),
-                        onPressed: () async {
-                          if (_formKey.currentState!.validate()) {
-                            setState(() {
-                              showSpinner = true;
-                            });
-
-                            try {
-                              await _auth
-                                  .createUserWithEmailAndPassword(
-                                      email: email, password: password)
-                                  .then((result) {
-                                dbRef.child(result.user!.uid).set({
-                                  "email": email,
-                                  "name": name,
-                                }).then((res) {
-                                  User? loggedUser =
-                                      FirebaseAuth.instance.currentUser;
-                                  Navigator.pushAndRemoveUntil(
-                                      context,
-                                      MaterialPageRoute(
-                                          builder: (context) =>
-                                              Home(uid: loggedUser!.uid)),
-                                      (Route<dynamic> route) => false);
-                                });
-                              });
-
-                              setState(() {
-                                showSpinner = false;
-                              });
-                            } catch (e) {
-                              String errorMessage = e.toString();
-
-                              setState(() {
-                                showSpinner = false;
-                              });
-
-                              kShowToast(errorMessage);
-                            }
-                          }
-                        },
+                        onPressed: performSignUp,
                         child: const Text('Sign Up'),
                       ),
                     ],
@@ -194,5 +176,49 @@ class _SignUpState extends State<SignUp> {
         ),
       ),
     );
+  }
+
+  void _togglePasswordView() {
+    setState(() {
+      _isHidden = !_isHidden;
+    });
+  }
+
+  performSignUp() async {
+    if (_formKey.currentState!.validate()) {
+      setState(() {
+        showSpinner = true;
+      });
+
+      try {
+        await _auth
+            .createUserWithEmailAndPassword(email: email, password: password)
+            .then((result) {
+          dbRef.child(result.user!.uid).set({
+            "email": email,
+            "name": name,
+          }).then((res) {
+            User? loggedUser = FirebaseAuth.instance.currentUser;
+            Navigator.pushAndRemoveUntil(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => Home(uid: loggedUser!.uid)),
+                (Route<dynamic> route) => false);
+          });
+        });
+
+        setState(() {
+          showSpinner = false;
+        });
+      } catch (e) {
+        String errorMessage = e.toString();
+
+        setState(() {
+          showSpinner = false;
+        });
+
+        kShowToast(errorMessage);
+      }
+    }
   }
 }
